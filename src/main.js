449f498b7727aa1c1403ebfe9039e81a6252772f
ashaunthomas <ashaunthomas@siu.edu>
2017-09-28 14:30:57 -0500

    menu stuff

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow =  electron.BrowserWindow;
-
+const Menu = electron.Menu;
+const name = electron.app.getName();
 app.on('ready', _ => {
-    new BrowserWindow({
-        height:400,
-        width:600
-    });
+    new BrowserWindow();
+    const template = [
+        {
+            label:name,
+            submenu: [{
+                label: `About ${name}`
+            }]
+        }
+    ]
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 });
